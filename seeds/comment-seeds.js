e12eb15d32127fae6700ebed94abf65e19a2f9ca
Admin <admin@admins-mbp.lan>
2022-02-09 21:57:05 -0800

    updated the server and basic mysql stuff

---
+++ seeds/comment-seeds.js
@@ -0,0 +1,7 @@
+const { Post } = require('../models');
+const postData = {
+    comment_text: `Meh. I wasn't too impressed by the addition of the new code lines. Very confusing`,
+    user_id: 1,
+    post_id: 1,
+
+}
